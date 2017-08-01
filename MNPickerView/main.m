294a81085f7c855f04d9e76708cce3ef2d7d3cf5
maning <ning.ma@12301.cn>
2017-08-01 10:37:04 +0800

    Initial Commit

---
+++ MNPickerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MNPickerView
+//
+//  Created by 马宁 on 2017/8/1.
+//  Copyright © 2017年 MaNing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
